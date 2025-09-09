4816d3b7ebd79fe13948e8d29711a306b1cd8e85
Sergei Akhremenko <sergeakhremenko@gmail.com>
2025-09-09 21:29:53 +0300

    up

--- assets/NewBookmark-kxtN-vgk.js
+++
@@ -1,1 +0,0 @@
-import{p as n,S as d,k,a6 as f,O as l}from"./index-BD4lsSGV.js";import{B as u}from"./BookmarkForm-J-33EAbZ.js";import"./Input-_VC7eulN.js";function B(a,o){n(o,!1);const t=()=>{l()},s=({title:m,url:r,description:i,tags:p})=>{f({title:m,url:r,description:i,tags:p.map(e=>e.trim())}),t()};d(),u(a,{onSubmit:s}),k()}export{B as default};
