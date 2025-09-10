969691f8e5259cbbafd3866c073b6bf7f44d2e59
Sergei Akhremenko <sergeakhremenko@gmail.com>
2025-09-10 21:42:43 +0300

    fix

---
+++ assets/NewNote-8qCFKGqJ.js
@@ -0,0 +1,1 @@
+import{p as r,S as d,k as f,a6 as l,O as u}from"./index-BZKyeLya.js";import{N}from"./NoteForm-A21Qm27d.js";import"./Input-lAG53kpe.js";function S(a,o){r(o,!1);const t=()=>{u()},s=({title:e,url:m,description:i,tags:p})=>{l({title:e,url:m,description:i,tags:p.map(n=>n.trim())}),t()};d(),N(a,{onSubmit:s}),f()}export{S as default};
