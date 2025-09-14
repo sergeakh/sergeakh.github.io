83854e1b645fc3df001c59a958a026607837c7b1
Sergei Akhremenko <sergeakhremenko@gmail.com>
2025-09-15 00:14:56 +0300

    fix

--- assets/NewNote-BB64RN-H.js
+++
@@ -1,1 +0,0 @@
-import{p as r,S as d,k as f,a6 as l,O as u}from"./index-Bla1u_Yh.js";import{N}from"./NoteForm-ERMkMbw5.js";import"./Input-CVsWIXj5.js";function S(a,o){r(o,!1);const t=()=>{u()},s=({title:e,url:m,description:i,tags:p})=>{l({title:e,url:m,description:i,tags:p.map(n=>n.trim())}),t()};d(),N(a,{onSubmit:s}),f()}export{S as default};
