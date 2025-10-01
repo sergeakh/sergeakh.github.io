d07b1784a6c89b323029231cfdc331adf9a669ee
Sergei Akhremenko <sergeakhremenko@gmail.com>
2025-10-02 02:02:51 +0300

    up

---
+++ a/VWDyBWg3.js
@@ -0,0 +1,1 @@
+import{p,C as f,z as u,q as l,r as N,m as g,g as b,y as h,d as v,n as y,T as C}from"./tf0cJmoX.js";import{N as x}from"./B13vmQvq.js";import"./BbUUGm8r.js";function A(s,o){p(o,!0);const{isInitedNotes:a}=f(),{back:n}=u(),r=h(a),i=()=>{n()},d=async()=>{await C(),i()};var e=l(),c=N(e);{var m=t=>{x(t,{onSubmit:d})};g(c,t=>{b(r)&&t(m)})}v(s,e),y()}export{A as default};
